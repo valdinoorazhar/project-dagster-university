fef09f80786eaec14c66b0586e578a29b202a6a6
valdinoorazhar <43821385+valdinoorazhar@users.noreply.github.com>
2025-09-26 09:06:37 +0000

    add partition

---
+++ dagster_university/dagster_essentials/src/dagster_essentials/defs/partitions.py
@@ -0,0 +1,11 @@
+# src/dagster_essentials/defs/partitions.py
+import dagster as dg
+from dagster_essentials.defs.assets import constants
+
+start_date = constants.START_DATE
+end_date = constants.END_DATE
+
+monthly_partition = dg.MonthlyPartitionsDefinition(
+    start_date=start_date,
+    end_date=end_date
+)
