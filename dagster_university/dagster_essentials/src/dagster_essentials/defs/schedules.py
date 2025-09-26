433a6d328adffad341dd85e7cd62a0b7c929d092
valdinoorazhar <43821385+valdinoorazhar@users.noreply.github.com>
2025-09-26 09:05:10 +0000

    create a scheduled job

--- dagster_university/dagster_essentials/src/dagster_essentials/defs/schedules.py
+++ dagster_university/dagster_essentials/src/dagster_essentials/defs/schedules.py
@@ -1,8 +1,18 @@
 # src/dagster_essentials/defs/schedules.py
 import dagster as dg
+
+from dagster_essentials.defs.jobs import weekly_update_job
+
+trip_update_schedule = dg.ScheduleDefinition(
+    job=weekly_update_job,
+    cron_schedule="0 0 * * 1", # every monday at midnight
+)
+'''
+
 from dagster_essentials.defs.jobs import trip_update_job
 
 trip_update_schedule = dg.ScheduleDefinition(
     job=trip_update_job,
     cron_schedule="0 0 5 * *", # every 5th of the month at midnight
 )
+'''
