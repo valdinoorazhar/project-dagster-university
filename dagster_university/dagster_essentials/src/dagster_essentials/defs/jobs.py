1e57d00e6c1524d0436deb883b40a98e5207d344
valdinoorazhar <43821385+valdinoorazhar@users.noreply.github.com>
2025-11-10 00:38:12 +0000

    add monthly partition to trip update job

--- dagster_university/dagster_essentials/src/dagster_essentials/defs/jobs.py
+++ dagster_university/dagster_essentials/src/dagster_essentials/defs/jobs.py
@@ -1,5 +1,6 @@
 # src/dagster_essentials/defs/jobs.py
 import dagster as dg
+from dagster_essentials.defs.partitions import monthly_partition
 
 # select trips_by_week asset
 trips_by_week = dg.AssetSelection.assets("trips_by_week")
@@ -7,6 +8,7 @@ trips_by_week = dg.AssetSelection.assets("trips_by_week")
 # a job that includes all asset other than trips_by_week
 trip_update_job = dg.define_asset_job(
     name="trip_update_job",
+    partition_defs =  monthly_partition
     selection=dg.AssetSelection.all() - trips_by_week
 )
 
