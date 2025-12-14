e758855a3538041d9ea45ef74d9143b078608dc9
valdinoorazhar <43821385+valdinoorazhar@users.noreply.github.com>
2025-12-14 07:55:00 +0000

    add weekly partition on jobs.py

--- dagster_university/dagster_essentials/src/dagster_essentials/defs/jobs.py
+++ dagster_university/dagster_essentials/src/dagster_essentials/defs/jobs.py
@@ -1,6 +1,7 @@
 # src/dagster_essentials/defs/jobs.py
 import dagster as dg
 from dagster_essentials.defs.partitions import monthly_partition
+from dagster_essentials.defs.partitions import weekly_partition
 
 # select trips_by_week asset
 trips_by_week = dg.AssetSelection.assets("trips_by_week")
@@ -15,5 +16,6 @@ trip_update_job = dg.define_asset_job(
 # this job will materialize trips_by_week asset
 weekly_update_job = dg.define_asset_job(
     name="weekly_update_job",
+    partition_defs = weekly_partition
     selection=trips_by_week,
 )
