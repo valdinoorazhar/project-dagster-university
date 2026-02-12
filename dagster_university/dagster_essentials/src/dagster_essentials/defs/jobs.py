d482d06a34e40d1a14c96eb3acbd92901f5ba37f
valdinoorazhar <43821385+valdinoorazhar@users.noreply.github.com>
2026-02-12 14:40:31 +0000

    add job containing requests asset

--- dagster_university/dagster_essentials/src/dagster_essentials/defs/jobs.py
+++ dagster_university/dagster_essentials/src/dagster_essentials/defs/jobs.py
@@ -6,11 +6,14 @@ from dagster_essentials.defs.partitions import weekly_partition
 # select trips_by_week asset
 trips_by_week = dg.AssetSelection.assets("trips_by_week")
 
+#select adhoc_request asset
+adhoc_request = dg.AssetSelection.assets(["adhoc_request"])
+
 # a job that includes all asset other than trips_by_week
 trip_update_job = dg.define_asset_job(
     name="trip_update_job",
     partitions_def=monthly_partition,
-    selection=dg.AssetSelection.all() - trips_by_week
+    selection=dg.AssetSelection.all() - trips_by_week - adhoc_request
 )
 
 # this job will materialize trips_by_week asset
@@ -18,4 +21,10 @@ weekly_update_job = dg.define_asset_job(
     name="weekly_update_job",
     partitions_def=weekly_partition,
     selection=trips_by_week,
+)
+
+
+adhoc_request_job = dg.define_asset_job(
+    name="adhoc_request_job",
+    selection=adhoc_request,
 )
